7f7aaf5c8172d97cf94755b088597d13377291d9
Rajath N R <rajath.nr117@gmail.com>
2024-01-06 13:37:00 -0800

    Implemented "/api/theaterEmployee/updateMovie/:movieId" API

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,7 +4,7 @@ const db = require("../util/db-initializer.js");
 
 module.exports = (app) => {
   // Insert New Movies to DB
-  app.post("/api/theater_employee/insert_movie", (req, res) => {
+  app.post("/api/theaterEmployee/insertMovie", (req, res) => {
     const body = req.body;
 
     const datetime = new Date();
@@ -24,28 +24,27 @@ module.exports = (app) => {
     db.query(query, dataToInsert, (error, results) => {
       if (error) {
         console.error("Error inserting data:", error);
-        return;
       }
-      console.log("Inserted data successfully:", results);
     });
 
     res.json({ movieId: objectId.toString() });
   });
 
   // Fetch all the movies that are not deleted from the DB
-  app.get("/api/theater_employee/get_movies", async (req, res) => {
+  app.get("/api/theaterEmployee/getMovies", async (req, res) => {
     const movies = [];
+
+    const query = "SELECT * FROM movies WHERE deleted = false";
     await new Promise((resolve, reject) => {
-      db.query("SELECT * FROM movies WHERE deleted = false", (error, rows) => {
+      db.query(query, (error, rows) => {
         resolve(
           rows.forEach((row) => {
             const rec = {
-              "id": row.id,
+              id: row.id,
               "title:": row.title,
-              "image": row.image,
-              "addedDate": row.addedDate,
-              "addedBy": row.addedBy,
-              "deleted": row.deleted
+              image: row.image,
+              addedDate: row.addedDate,
+              addedBy: row.addedBy,
             };
             movies.push(rec);
           })
@@ -56,7 +55,7 @@ module.exports = (app) => {
   });
 
   // Update Movie metadata
-  app.put("/api/theater_employee/update_movie/:movieId", async (req, res) => {
+  app.put("/api/theaterEmployee/updateMovie/:movieId", async (req, res) => {
     const movieId = req.params.movieId;
     const body = req.body;
 
@@ -64,18 +63,39 @@ module.exports = (app) => {
 
     if ("title" in body) {
       dataToUpdate["title"] = body.title;
-    };
-    
+    }
+
     if ("image" in body) {
       dataToUpdate["image"] = body.image;
-    };
+    }
 
-    const query = "Update movies SET " + Object.keys(dataToUpdate).map(key => `${key} = ?`).join(", ") + " WHERE id = ?";
+    const query =
+      "Update movies SET " +
+      Object.keys(dataToUpdate)
+        .map((key) => `${key} = ?`)
+        .join(", ") +
+      " WHERE id = ?";
     const parameters = [...Object.values(dataToUpdate), movieId];
 
     await new Promise((resolve, reject) => {
-      db.query(query, parameters, resolve( () => {} ))
+      db.query(query, parameters, resolve());
     });
-    res.json({"message": "success"});
+    res.json({ message: "success" });
+  });
+
+  // Delete Movie
+  app.delete("/api/theaterEmployee/deleteMovie/:movieId", async (req, res) => {
+    const movieId = req.params.movieId;
+
+    const query = "Update movies SET deleted = ? WHERE id = ?";
+    const parameters = [1, movieId];
+
+    // const query = "DELETE FROM movies WHERE id = ?"
+    // const parameters = [movieId];
+
+    await new Promise((resolve, reject) => {
+      db.query(query, parameters, resolve());
+    });
+    res.json({ message: "success" });
   });
 };
