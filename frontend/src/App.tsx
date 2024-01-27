895cfb2ce55f6d858863ae35cc731a069395901b
Rajath N R <rajath.nr117@gmail.com>
2024-01-26 16:41:25 -0800

    Frontend Repo initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Landing from "./Landing";
+
+const App = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Landing.tsx
@@ -0,0 +1,5 @@
+function Landing() {
+  return <h1>Hello World</h1>;
+}
+
+export default Landing;
